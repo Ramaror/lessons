20350e4ec20acde95d053d7c0ef91cf829f7cbc7
Ramaror <mail.the.ramzan@mail.ru>
2022-09-01 22:36:40 +0300

    init commit

--- HomeWork9/homework3.01/loo.go
+++ HomeWork9/homework3.01/loo.go
@@ -5,6 +5,7 @@ import (
 	"awesomeProject/HomeWork9/homework3.01/Digit"
 	"fmt"
 	"math/rand"
+	"strings"
 )
 
 /*
@@ -15,11 +16,11 @@ import (
 *Digit() - возвращает случайное число строчного типа
 *Обе функции должны формировать результат с помощью функции Random
 из пакета wordz. При этом не внося никаких изменение в пакет wordz.
-выведите через fmt.Println результат функции City и Digit в файле main.go
+выведите через fmt.Println результат функции City и Digit в файле loo.go
 */
 /*
 С помощью утилиты go get, установите пакет для работы со строками xstrings.
-В файле main.go примените фунцию Shaffle
+В файле utils.go примените фунцию Shaffle
 из этого пакета к результату функции City().
 И угадайте, какое название города вывелось
 */
@@ -44,5 +45,9 @@ func Shuffle(str string) string {
 func main() {
 	fmt.Println("Random city:", Shuffle(City.City()))
 	fmt.Println(Digit.Digit())
-
+	words := strings.Fields("hello world in nighbor")
+	rand.Shuffle(len(words), func(i, j int) {
+		words[i], words[j] = words[j], words[i]
+	})
+	fmt.Println(words)
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module awesomeProject
 
 go 1.19
 
+require github.com/fatih/color v1.13.0
+
 require (
-	github.com/fatih/color v1.13.0 // indirect
-	github.com/gopherjs/gopherjs v0.0.0-20190411002643-bd77b112433e // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/therecipe/qt v0.0.0-20200904063919-c0c124a5770d // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 )
